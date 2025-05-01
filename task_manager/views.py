0d41911619de6e381cfcbccbc9abd377f2b14e56
Marat Mukhutdinov <kazanmarat2009@yandex.ru>
2025-05-01 08:47:42 +0300

    Step 2.0 - django-bootstrap5

--- task_manager/views.py
+++ task_manager/views.py
@@ -1,4 +1,10 @@
-from django.shortcuts import render
+# from django.shortcuts import render
 
-def index(request):
-    return render(request, 'index.html')
+# def index(request):
+#     return render(request, 'index.html')
+
+from django.views.generic import TemplateView
+
+
+class IndexView(TemplateView):
+    template_name = "index.html"
