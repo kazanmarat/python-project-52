631f1da1fddf5c2a129c6bf648789a48ec7480fd
Marat Mukhutdinov <kazanmarat2009@yandex.ru>
2025-05-20 14:19:35 +0300

    Step 4.2 - Modify tests

--- statuses/models.py
+++ statuses/models.py
@@ -2,9 +2,8 @@ from django.db import models
 from datetime import datetime
 from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
 class Status(models.Model):
-    # name = models.CharField(max_length=50)
     name = models.CharField(_("name"), max_length=50, unique=True)
     date_creation = models.DateTimeField(default=datetime.now)
 
--- statuses/tests.py
+++ statuses/tests.py
@@ -7,44 +7,43 @@ from django.urls import reverse
 class StatusTest(TestCase):
     fixtures = ['statuses/fixtures.json', 'statuses/fixtures_user.json',]
 
+    @classmethod
+    def setUpTestData(cls):
+        cls.initial_status_count = Status.objects.count()
+        cls.user = CustomUser.objects.get(pk=1)
+        cls.status = Status.objects.get(pk=1)
+        cls.status_list_url = reverse("status_list")
+        
     def setUp(self):
-        user = CustomUser.objects.get(pk=1)
-        self.client.get(reverse("login"))
-        self.client.force_login(user)
+        self.client.force_login(self.user)
 
     def test_status_list(self):
-        status_count = Status.objects.count()
-        status_list_url = reverse("status_list")
-        response = self.client.get(status_list_url)
+        response = self.client.get(self.status_list_url)
         statuses = response.context["statuses"]
         self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(status_list_url)
-        self.assertTrue(len(statuses) == status_count)
+        self.assertTemplateUsed(self.status_list_url)
+        self.assertTrue(len(statuses) == self.initial_status_count)
 
     def test_status_create(self):
-        initial_count = Status.objects.count()
-        status_list_url = reverse("status_list")
-        create_url = reverse("status_create")
-
         # create status
+        create_url = reverse("status_create")
         response = self.client.get(create_url)
         self.assertEqual(response.status_code, 200)
         status_data = {'name': 'test status 3'}
         response = self.client.post(create_url, status_data)
         self.assertEqual(response.status_code, 302)
-        self.assertRedirects(response, status_list_url)
+        self.assertRedirects(response, self.status_list_url)
 
         # check added status
-        response = self.client.get(status_list_url)
+        response = self.client.get(self.status_list_url)
         statuses = response.context["statuses"]
-        self.assertTrue(len(statuses) == initial_count + 1)
+        self.assertContains(response, 'test status 3')
+        self.assertTrue(len(statuses) == self.initial_status_count + 1)
 
     def test_status_update(self):
-        status = Status.objects.get(pk=1)
-        old_status_name = status.name
+        old_status_name = self.status.name
         new_status_name = 'new test status'
-        update_url = reverse("status_update", kwargs={"pk": status.id})
-        status_list_url = reverse("status_list")
+        update_url = reverse("status_update", kwargs={"pk": self.status.id})
 
         # update status
         response = self.client.get(update_url)
@@ -52,29 +51,26 @@ class StatusTest(TestCase):
         status_data = {'name': new_status_name}
         response = self.client.post(update_url, status_data)
         self.assertEqual(response.status_code, 302)
-        self.assertRedirects(response, status_list_url)
+        self.assertRedirects(response, self.status_list_url)
 
         # check updated status
-        response = self.client.get(status_list_url)
+        response = self.client.get(self.status_list_url)
         self.assertContains(response, new_status_name)
         self.assertNotContains(response, old_status_name)
 
     def test_status_delete(self):
-        status = Status.objects.get(pk=1)
-        status_name = status.name
-        initial_count = Status.objects.count()
-        delete_url = reverse("status_delete", kwargs={"pk": status.id})
-        status_list_url = reverse("status_list")
+        status_name = self.status.name
+        delete_url = reverse("status_delete", kwargs={"pk": self.status.id})
 
         # delete status
         response = self.client.get(delete_url)
         self.assertEqual(response.status_code, 200)
         response = self.client.post(delete_url)
         self.assertEqual(response.status_code, 302)
-        self.assertRedirects(response, status_list_url)
+        self.assertRedirects(response, self.status_list_url)
 
         # check deleted status
-        response = self.client.get(status_list_url)
+        response = self.client.get(self.status_list_url)
         statuses = response.context["statuses"]
-        self.assertTrue(len(statuses) == initial_count - 1)
+        self.assertTrue(len(statuses) == self.initial_status_count - 1)
         self.assertNotContains(response, status_name)
