aecec7bc7a1c4a982fe1d0b9ecd72f86318873ef
Marat Mukhutdinov <kazanmarat2009@yandex.ru>
2025-05-21 08:39:40 +0300

    Step 3.6 - modify to admin app

--- statuses/models.py
+++ statuses/models.py
@@ -7,5 +7,9 @@ class Status(models.Model):
     name = models.CharField(_("name"), max_length=50, unique=True)
     date_creation = models.DateTimeField(default=datetime.now)
 
+    class Meta:
+        verbose_name = _('Status')
+        verbose_name_plural = _('Statuses')
+
     def __str__(self):
         return self.name
