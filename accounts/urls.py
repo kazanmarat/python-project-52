1b83c0c636faa0dcae224db485e79ee88bae113a
Marat Mukhutdinov <kazanmarat2009@yandex.ru>
2025-05-29 08:12:38 +0300

    fix routes

--- accounts/urls.py
+++ accounts/urls.py
@@ -9,6 +9,6 @@ from .views import (
 urlpatterns = [
     path('', UserListView.as_view(), name='user_list'),
     path('create/', SignUpView.as_view(), name='user_create'),
-    path('<int:pk>/update', UserUpdateView.as_view(), name='user_update'),
-    path('<int:pk>/delete', UserDeleteView.as_view(), name='user_delete'),
+    path('<int:pk>/update/', UserUpdateView.as_view(), name='user_update'),
+    path('<int:pk>/delete/', UserDeleteView.as_view(), name='user_delete'),
 ]
--- statuses/urls.py
+++ statuses/urls.py
@@ -8,7 +8,7 @@ from .views import (
 
 urlpatterns = [
     path('', StatusListView.as_view(), name='status_list'),
-    path('create', StatusCreateView.as_view(), name='status_create'),
-    path('<int:pk>/update', StatusUpdateView.as_view(), name='status_update'),
-    path('<int:pk>/delete', StatusDeleteView.as_view(), name='status_delete'),
+    path('create/', StatusCreateView.as_view(), name='status_create'),
+    path('<int:pk>/update/', StatusUpdateView.as_view(), name='status_update'),
+    path('<int:pk>/delete/', StatusDeleteView.as_view(), name='status_delete'),
 ]
